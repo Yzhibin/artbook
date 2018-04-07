026c647cb391fbc5cddd49cf574367979395c116
Zhibin Yu <zhibinyu97@gmail.com>
2018-04-07 21:41:34 +0800

    Authentication Done

--- node/api/controllers/userController.js
+++ node/api/controllers/userController.js
@@ -1,4 +1,5 @@
 'use strict';
+var bcrypt = require('bcrypt')
 
 //TODO
 var mongoose = require('mongoose'),
@@ -24,7 +25,7 @@ Task.find({}, function(err, task) {
  * email
  * name
  * avatar
- * 
+ * password
  */
 exports.createUser = function (req, res) {
   var userInfo = req.body
@@ -36,17 +37,36 @@ exports.createUser = function (req, res) {
   }
   var userOffChain = {
     id: userInfo.email,
-    avatar: userInfo.avatar
+    avatar: userInfo.avatar,
   }
 
-  var adminHandlesNewUser = new userHandler('admin@artbook')
-  adminHandlesNewUser.createUser(userOnChain)
+  bcrypt.genSalt(function (err, salt) {
+    if (err) {
+      console.log('bcrypt.genSalt ERR')
+    }
+    console.log(`Salt: ${salt}`)
+    userOffChain.salt = salt
+    console.log(`userInfo.pwd: ${userInfo.password}`)
+    userInfo.password = userInfo.password + salt
+    
+    bcrypt.hash(userInfo.password, 10, function (err, hashed) {
+      if (err) {
+        console.log('bcrypt.hash ERR')
+      }
+      console.log(`Hash: ${hashed} `)
+      userOffChain.password = hashed
 
-  var new_user = new User(userOffChain)
-  new_user.save(function (err, user) {
-    if (err)
-      res.send(err);
-    res.json(user);
+      var adminHandlesNewUser = new userHandler('admin@artbook')
+      // adminHandlesNewUser.createUser(userOnChain)
+
+      var new_user = new User(userOffChain)
+      console.log(new_user)
+      new_user.save(function (err, user) {
+        if (err)
+          res.send(err);
+        res.json(user);
+      })
+    })
   })
 };
 
--- node/server.js
+++ node/server.js
@@ -5,19 +5,63 @@ var express = require('express'),
   Task = require('./api/models/schema'), //created model loading here
   bodyParser = require('body-parser');
 
-  
-
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/artbookDB'); 
-  
+mongoose.connect('mongodb://localhost/artbookDB');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Auth
+var passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy,
+  bcrypt = require('bcrypt')
+
+app.use(require('express-session')({
+  secret: 'session secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var User = require('./api/models/schema').User;
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+},
+  function (username, password, done) {
+    console.log('LOG2')
+    User.findOne({ id: username }, function (err, user) {
+      console.log(user)
+      if (err) { return done(err) }
+      if (!user) {
+        console.log('No such user')
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      bcrypt.compare(password + user.salt, user.password, function (err, res) {
+        if (!res) {
+          console.log('Incorrect pwd')
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+    })
+  }))
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findOne({ id: id }, function (err, user) {
+    done(err, user);
+  });
+});
 
 
 var routes = require('./api/routes/routes'); //importing route
-routes(app); //register the route
+routes(app, passport); //register the route
 
 
 app.listen(port);
